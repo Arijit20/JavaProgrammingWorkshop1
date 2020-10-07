12db415b203f744d6b6a549cce8a53625611d4f7
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-07 09:36:48 +0530

    [Arijit]Added : Choose Char For Player

--- src/com/example/TicTacToeGame.java
+++ src/com/example/TicTacToeGame.java
@@ -1,13 +1,15 @@
 package com.example;
+import java.util.Scanner;
 
 public class TicTacToeGame {
 
 	private char[] board = new char[10];
-
+	private char player, computer;
+	Scanner in = new Scanner(System.in);
+	
 	public char[] initializeBoard() {
 		for (char ch : board) {
 			board[ch] = ' ';
-
 		}
 		return board;
 	}
@@ -15,9 +17,25 @@ public class TicTacToeGame {
 	public TicTacToeGame() {
 		initializeBoard();
 	}
-
+	public void chooseLetter() {
+		System.out.println("Enter either x or o what yoy want to choose");
+		int flag = 0;
+		while(flag == 0) {
+		char option = in.next().charAt(0);
+		if(option == 'o' || option == 'x') {
+			player = option;
+			computer = player == 'o' ? 'x' : 'o';
+			flag = 1;
+		}
+		else
+			System.out.println("Enter either o or x");
+	}
+		System.out.println("Player :" + player);
+		System.out.println("Computer : " +computer);
+}
 	public static void main(String[] args) {
 		System.out.println("Welcome to the game");
 		TicTacToeGame game = new TicTacToeGame();
+		game.chooseLetter();
 	}
 }
