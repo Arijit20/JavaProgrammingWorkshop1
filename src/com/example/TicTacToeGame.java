e00cb4be5b67e939d6da64f28c21c1275743b9a4
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-10-09 15:54:31 +0530

    [Arijit]Modified : Play Till The End

--- src/com/example/TicTacToeGame.java
+++ src/com/example/TicTacToeGame.java
@@ -42,7 +42,7 @@ public class TicTacToeGame {
 				|| (board[1] == ch && board[4] == ch && board[7] == ch)
 				|| (board[2] == ch && board[5] == ch && board[8] == ch)
 				|| (board[3] == ch && board[6] == ch && board[9] == ch)) {
-			System.out.println("congrats");
+			System.out.println("The game is over");
 			return true;
 		} else
 			return false;
@@ -76,35 +76,35 @@ public class TicTacToeGame {
 		if (isAvailable(1) && ((board[2] == ch && board[3] == ch) || (board[4] == ch && board[7] == ch)
 				|| (board[5] == ch && board[9] == ch)))
 			return 1;
-		
+
 		if (isAvailable(2) && ((board[1] == ch && board[3] == ch) || (board[5] == ch && board[8] == ch)))
 			return 2;
-		
+
 		if (isAvailable(3) && ((board[2] == ch && board[1] == ch) || (board[6] == ch && board[9] == ch)
 				|| (board[5] == ch && board[7] == ch)))
 			return 3;
-		
+
 		if (isAvailable(4) && ((board[1] == ch && board[7] == ch) || (board[5] == ch && board[6] == ch)))
 			return 4;
-		
+
 		if (isAvailable(5) && ((board[1] == ch && board[9] == ch) || (board[3] == ch && board[7] == ch)
 				|| (board[2] == ch && board[8] == ch) || (board[4] == ch && board[6] == ch)))
 			return 5;
-		
+
 		if (isAvailable(6) && ((board[3] == ch && board[9] == ch) || (board[5] == ch && board[4] == ch)))
 			return 6;
-		
+
 		if (isAvailable(7) && ((board[1] == ch && board[4] == ch) || (board[9] == ch && board[8] == ch)
 				|| (board[5] == ch && board[3] == ch)))
 			return 7;
-		
+
 		if (isAvailable(8) && ((board[2] == ch && board[5] == ch) || (board[7] == ch && board[9] == ch)))
 			return 8;
-		
+
 		if (isAvailable(9) && ((board[7] == ch && board[8] == ch) || (board[3] == ch && board[6] == ch)
 				|| (board[5] == ch && board[1] == ch)))
 			return 9;
-		
+
 		else
 			return 0;
 
@@ -126,32 +126,32 @@ public class TicTacToeGame {
 
 	public int blockPlayer() {
 		int position = ifPossibleToWin(player);
-		if (position == 0) 
+		if (position == 0)
 			position = takeCorner();
 		return position;
 	}
 
 	public int takeCorner() {
-		if(isAvailable(1))
+		if (isAvailable(1))
 			return 1;
-		else if(isAvailable(3))
+		else if (isAvailable(3))
 			return 3;
-		else if(isAvailable(7))
+		else if (isAvailable(7))
 			return 7;
-		else if(isAvailable(9))
+		else if (isAvailable(9))
 			return 9;
-		else 
+		else
 			return midOrSidePosition();
 	}
 
 	public int midOrSidePosition() {
-		if(isAvailable(5))
+		if (isAvailable(5))
 			return 5;
 		else {
 			int flag = 0, position = 0;
-			while(flag == 0) {
-				position = ((int)Math.floor(Math.random()* 10) % 9) + 1;
-				if(isAvailable(position))
+			while (flag == 0) {
+				position = ((int) Math.floor(Math.random() * 10) % 9) + 1;
+				if (isAvailable(position))
 					flag = 1;
 			}
 			return position;
